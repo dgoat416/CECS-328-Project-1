7930d8768c90ac23f67be72a4e0ceb8a0efd9469
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-08-24 11:46:06 -0700

    Create Solution.java
    - This class will house the solution to Project 1

---
+++ src/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * Method to produce the solution to Project 1
+ * @author Deron Washington II
+ * Date Started: 8/24/20 @ 11:21am
+ *
+ */
+//import java.BigInteger
+
+public class Solution 
+{
+	/** 
+	 * Method to find the fraction a/b that is approximately
+	 * equivalent to sqrt(N/D)
+	 * meaning a/b must be the fraction in the tree such that
+	 * abs(Da^2 - Nb^2) <= b
+	 * @param N = Numerator
+	 * @param D = Denominator
+	 */
+	public static void findFractionAB(int N, int D)
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
