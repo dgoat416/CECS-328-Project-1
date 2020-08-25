7de9c91bae8b543893aca8df989449b4dd4c5b77
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-08-25 08:19:37 -0700

    Create findFraction function
    - Still trying to decide the best way to go about this considering:
    only go through the side of the tree that I need to based on if M / N is  greater than,  less than or equal to the curent value of the tree we are at

--- src/Solution.java
+++ src/Solution.java
@@ -1,11 +1,17 @@
+import java.awt.Point;
+import java.io.File;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Method to produce the solution to Project 1
  * @author Deron Washington II
  * Date Started: 8/24/20 @ 11:21am
  *
  */
-//import java.BigInteger
-
 public class Solution 
 {
 	/** 
@@ -16,15 +22,47 @@ public class Solution
 	 * @param N = Numerator
 	 * @param D = Denominator
 	 */
-	public static void findFractionAB(int N, int D)
+	public static void findFractionAB(BigInteger N, BigInteger D)
 	{
+		// store the value we are looking for N / B
+		BigDecimal findAB = new BigDecimal(N).divide(new BigDecimal(D), RoundingMode.);
+		ArrayList<BigDecimal> fractionalTree = new ArrayList<BigDecimal>();
+		ArrayList<Point> points = new ArrayList<Point>();
+		points.add(new Point(0, 1));
+		points.add(new Point(1,0));
 		
+		
+		
+//		for (int i = 0; i < 7; i++)
+			
 	}
 	
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-
+		Scanner scan = null;
+		BigInteger N = null;
+		BigInteger D = null;
+		
+		try
+		{
+			File inFile = new File("input_1.txt");
+			scan = new Scanner(inFile);
+			
+			// get the numerator (N) and denominator (D)
+			N = scan.nextBigInteger();
+			D = scan.nextBigInteger();
+			
+			// where the magic happens
+			findFractionAB(N, D);
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+			
+		
 	}
 
 }
